e9384e06cdec29a2be9dba5483d2ab4daf86a42c
singhkunal2050 <singhkunal2050@gmail.com>
2020-11-14 14:10:53 +0530

    optimised map

--- footer.php
+++ footer.php
@@ -72,8 +72,8 @@
 					</ul>
 				</div>
 				<!-- widget -->
-				<div class="col-sm-6 col-lg-6 footer-widget">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15135.44393481443!2d73.81149522054879!3d18.489955098953452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bfe79bf771d7%3A0xac81e25be4cc4bf!2sKarve%20Nagar%2C%20Pune%2C%20Maharashtra%20411052!5e0!3m2!1sen!2sin!4v1574428292244!5m2!1sen!2sin" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+				<div class="col-sm-6 col-lg-6 footer-widget" >
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15135.44393481443!2d73.81149522054879!3d18.489955098953452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bfe79bf771d7%3A0xac81e25be4cc4bf!2sKarve%20Nagar%2C%20Pune%2C%20Maharashtra%20411052!5e0!3m2!1sen!2sin!4v1574428292244!5m2!1sen!2sin" width="100%" height="350" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 				</div>
 			</div>
 		</div>
